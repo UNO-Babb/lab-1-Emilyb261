72063163710e8e44504a169e1ef66988ff1681f4
Emilyb261 <ebillings@unomaha.edu>
2025-08-29 12:24:42 -0500

    Emily Billings MadLib.py

--- MadLib.py
+++ MadLib.py
@@ -6,8 +6,20 @@
 def main():
   print("Madlib")
   #Ask user for words
+  noun1= input("Enter a noun: ")
+  p_noun= input("choose a plural noun (a person, place, or thing): ")
+  noun2= input("choose another plural noun (more than one thing): ")
+  place= input("name a place: ")
+  adjective1= input("choose an adjective (a describing word): ")
+  noun3= input("choose one more noun: ")
 
   #Print the story with the user supplied words.
+print("School was "+adjective1+"!")
+  print("I had class with "+noun1+"!")
+  print("In school I had recess with "+p_noun+"!")
+  print("I had lunch with "+ noun3+"!")
+  print("Lunch food was "+adjective1+"!")
+  print("My day was "+adjective1+"!")
 
 
 
